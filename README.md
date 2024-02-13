5ca35aaa9f87d2120c0a65355ca3219718aa421b
s. galiamov <sgalyamov@gmail.com>
2024-02-13 16:03:00 +0100

    start

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 💤 LazyVim
 
 My config for [LazyVim](https://github.com/LazyVim/LazyVim).
+
+``` bash
+
+git clone -b main https://github.com/sgaliamov/lazy-vim ./nvim
+
+```
--- init.lua
+++ init.lua
@@ -1,8 +1,17 @@
-if vim.g.vscode then
-    -- VSCode extension
-
-    vim.opt.clipboard = 'unnamedplus'
-else
-    require("config.lazy")
+-- Install lazy vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+if vim.g.vscode then
+  -- VSCode extension
+
+  vim.opt.clipboard = 'unnamedplus'
+else
+
+end
