2935366325265811a249a940c722aea554a2e93b
s. galiamov <sgalyamov@gmail.com>
2024-03-03 22:43:00 +0100

    tired treesitter

--- lua/plugins/nvim/lsp/nvim-treesitter.lua
+++ lua/plugins/nvim/lsp/nvim-treesitter.lua
@@ -1,15 +1,15 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   event = 'VeryLazy',
-  enabled = false, -- todo: fix it
+  enabled = false, -- todo: does not work with telescope
   opts = {
     ensure_installed = {
-      -- 'json',
+      'json',
       'lua',
-      -- 'markdown_inline',
-      -- 'rust',
-      -- 'xml',
-      -- 'yaml',
+      'markdown_inline',
+      'rust',
+      'xml',
+      'yaml',
     },
     higlights = {
       enable = true
