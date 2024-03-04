cf96eac71f22c1e770ef6ea5240dbb4a28ae5688
s. galiamov <sgalyamov@gmail.com>
2024-03-04 15:21:07 +0100

    keys for vs code

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,9 +1,19 @@
-local map_keys = require('keymaps.utils').map_keys
+require('keymaps.utils').map_keys({
+  ['<Esc>'] = { '<Esc>:noh<cr>', 'Cancel search highlighting with ESC' },
+  ['jj'] = { '<Esc>', 'Fast excape', m = 'i', opts = { remap = true } },
+  ['<leader><leader>'] = { '<nop>', 'Diable space in nomal mode' },
 
-local mappings = {
-}
+  -- Better existing remappings.
+  ['<C-d>'] = { '<C-d>zz', 'Scroll down & center' },
+  ['<C-u>'] = { '<C-u>zz', 'Scroll up & center' },
 
-map_keys(mappings)
+  -- Better yanking.
+  ['<leader>p'] = { '"_dP', 'Replace without yanking', m = 'v' },
+  ['<leader>c'] = { '"_ciw', 'Change inner word without yanking', m = 'n' },
+  ['<leader>d'] = { '"_d', 'Delete without yanking' },
+  ['x'] = { '"_x', 'Remove without yanking' },
+  ['<leader>yy'] = { '"+yiw', 'Yank inner word in clipboard' },
+})
 
 if vim.g.vscode then
   require('keymaps.vscode')
--- lua/keymaps/nvim.lua
+++ lua/keymaps/nvim.lua
@@ -1,33 +1,20 @@
-local map_keys = require('keymaps.utils').map_keys
+local vinc = { '!', 'n', 'v' }
 
-local all_modes = { '!', 'n', 'v' }
-
-map_keys({
-  ['<Esc>'] = { '<Esc>:noh<cr>', 'Cancel search highlighting with ESC' },
-  ['jj'] = { '<Esc>', 'Fast excape', m = 'i', opts = { remap = true } },
-  ['<leader>L'] = { ':Lazy<cr>', 'Lazy UI' }, -- todo: should be in the plug config.
+require('keymaps.utils').map_keys({
+  ['<leader>L'] = { ':Lazy<cr>', 'Lazy UI' },      -- todo: should be in the plug config.
+  ['<A-q>'] = { '<cmd>qa<cr>', 'Quit', m = vinc }, -- todo: find a way to use Ctrl
+  ['<A-s>'] = { '<cmd>wa<cr>', 'Save all', m = vinc },
 
   -- Toggle languages.
-  -- ['<leader>l'] = { ':noh<cr>', 'Keyboard layouts...' },                          -- todo: find better way to set a group name.
+  -- ['<leader>l'] = { ':noh<cr>', 'Keyboard layouts...' },                   -- todo: find better way to set a group name.
   ['<leader>lr'] = { ':set keymap=russian-jcukenwin<cr>', 'Russian layout' }, -- todo: implement toggle
   ['<leader>ld'] = { ':set keymap=""<cr>', 'Default layout' },                -- todo: in vscode the layout is not working in insert mode.
 
-  ['<F4>'] = { '<cmd>WhichKey<cr>', 'Which key', m = all_modes },
-  ['<A-q>'] = { '<cmd>qa<cr>', 'Quit', m = all_modes }, -- todo: find a way to use Ctrl
-  ['<A-s>'] = { '<cmd>wa<cr>', 'Save all', m = all_modes },
-
-  -- Better existing remappings.
-  ['<C-d>'] = { '<C-d>zz', 'Scroll down & center' },
-  ['<C-u>'] = { '<C-u>zz', 'Scroll up & center' },
+  -- Better yanking.
+  ['<leader>y'] = { '"+y', 'Yank in clipboard' },
+  ['<leader>Y'] = { '"+Y', 'Yank line in clipboard' },
 
   -- Better text editing.
-  ['<leader>p'] = { '"_dP', 'Replace without yanking', m = 'v' },
-  ['<leader>c'] = { '"_ciw', 'Change inner word without yanking', m = 'n' },
-  ['<leader>d'] = { '"_d', 'Delete without yanking' },
-  ['<leader>y'] = { '"+y', 'Yank in clipboard' },
-  ['<leader>yy'] = { '"+yiw', 'Yank inner word in clipboard' },
-  ['<leader>Y'] = { '"+Y', 'Yank line in clipboard' },
-  ['x'] = { '"_x', 'Remove without yanking' },
   ['<A-k>'] = { ':m -2<cr>gv=gv', 'Move text up' },
   ['<A-j>'] = { ':m +1<cr>gv=gv', 'Move text down' },
 
