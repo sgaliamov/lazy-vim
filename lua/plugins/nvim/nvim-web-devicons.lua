7c40eca422614c4fc6c9ce27aeb967fbef228fb6
s. galiamov <sgalyamov@gmail.com>
2024-03-09 01:06:38 +0100

    nvim-web-devicons

---
+++ lua/plugins/nvim/nvim-web-devicons.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  event = 'VeryLazy',
+  opts = {
+    override = {
+      gql = { -- todo: test or use require("nvim-web-devicons").set_icon
+        icon = "",
+        color = "#e535ab",
+        cterm_color = "199",
+        name = "GraphQL",
+      }
+    }
+  }
+}
