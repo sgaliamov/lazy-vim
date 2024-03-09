955bfa002285e660e72fb3855da36b6fee99aed3
s. galiamov <sgalyamov@gmail.com>
2024-03-09 01:05:53 +0100

    lualine

---
+++ lua/plugins/nvim/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      theme = 'everforest',
+      disabled_filetypes = { "NvimTree" }
+    },
+  }
+}
