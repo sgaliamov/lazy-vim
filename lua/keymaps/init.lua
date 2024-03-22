364ec3d667c6d1e375dc6b6b6860ea805ab300ad
s. galiamov <sgalyamov@gmail.com>
2024-03-22 19:15:09 +0100

    new surround

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -6,6 +6,34 @@ require('keymaps.utils').map_keys {
   ['<leader>c'] = { '"_ciw', 'Change inner word without yanking', m = 'n' },
   ['<leader>x'] = { '"_x', 'Remove without yanking', m = 'n' },
   ['<leader>d'] = { '"_d', 'Delete without yanking' },
+  --   " surrounding workaround
+  -- nnoremap s <NOP>
+  -- nnoremap sd <NOP>
+  -- vnoremap s <NOP>
+  --
+  -- " surround inner word
+  -- nnoremap s" ciw"<C-r>""<Esc>
+  -- nnoremap s' ciw'<C-r>"'<Esc>
+  -- nnoremap s, ciw<<C-r>"><Esc>
+  -- nnoremap s9 ciw(<C-r>")<Esc>
+  -- nnoremap s[ ciw[<C-r>"]<Esc>
+  -- nnoremap s{ ciw{<C-r>"}<Esc>
+  --
+  -- " surround visual selected text
+  -- vnoremap s" c"<C-r>""<Esc>
+  -- vnoremap s' c"<C-r>"'<Esc>
+  -- vnoremap s, c<<C-r>"><Esc>
+  -- vnoremap s9 c(<C-r>")<Esc>
+  -- vnoremap s[ c[<C-r>"]<Esc>
+  -- vnoremap s{ c{<C-r>"}<Esc>
+  --
+  -- " delete surround
+  -- nnoremap sd" di"h"_2s<C-r>"<Esc>
+  -- nnoremap sd' di'h"_2s<C-r>"<Esc>
+  -- nnoremap sd, di<h"_2s<C-r>"<Esc>
+  -- nnoremap sd9 di(h"_2s<C-r>"<Esc>
+  -- nnoremap sd[ di[h"_2s<C-r>"<Esc>
+  -- nnoremap sd{ di{h"_2s<C-r>"<Esc>
 }
 
 if vim.g.vscode then
