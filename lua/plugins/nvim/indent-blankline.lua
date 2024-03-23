83c1cb3613bd5e4d0d03edaf44ac5cfc5c8f87da
s. galiamov <sgalyamov@gmail.com>
2024-03-23 17:50:39 +0100

    better blankline

--- lua/plugins/nvim/indent-blankline.lua
+++ lua/plugins/nvim/indent-blankline.lua
@@ -2,22 +2,39 @@
 Vertical lines on scopes 
 ]]
 
+local highlight = {
+  'RainbowRed',
+  'RainbowYellow',
+  'RainbowBlue',
+  'RainbowOrange',
+  'RainbowGreen',
+  'RainbowViolet',
+  'RainbowCyan',
+}
+
 return {
   'lukas-reineke/indent-blankline.nvim',
   main = 'ibl',
   event = 'VeryLazy',
-  config = function()
-    local highlight = {
-      'CursorColumn',
-      'Whitespace',
-    }
-    require('ibl').setup {
-      indent = { highlight = highlight, char = '' },
-      whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-      },
-      scope = { enabled = false },
-    }
+  opts = {
+    indent = {
+      highlight = highlight,
+      char = '│',
+    },
+  },
+  config = function(_, opts)
+    local hooks = require 'ibl.hooks'
+
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+      vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+      vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+      vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+      vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+      vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+      vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+    end)
+
+    require('ibl').setup(opts)
   end,
 }
