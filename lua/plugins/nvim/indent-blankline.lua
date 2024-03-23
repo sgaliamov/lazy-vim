fb04b718d7e366d6ad042f8112cc8879f6bdb798
s. galiamov <sgalyamov@gmail.com>
2024-03-23 01:04:41 +0100

    indent-blankline

---
+++ lua/plugins/nvim/indent-blankline.lua
@@ -0,0 +1,23 @@
+--[[ 
+Vertical lines on scopes 
+]]
+
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  event = 'VeryLazy',
+  config = function()
+    local highlight = {
+      'CursorColumn',
+      'Whitespace',
+    }
+    require('ibl').setup {
+      indent = { highlight = highlight, char = '' },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = { enabled = false },
+    }
+  end,
+}
