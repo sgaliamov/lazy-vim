0809df2daac946fbcf93f7bbcc30725415011e1a
s. galiamov <sgalyamov@gmail.com>
2024-03-31 15:13:45 +0200

    lighter color for indent-blankline

--- lua/plugins/nvim/indent-blankline.lua
+++ lua/plugins/nvim/indent-blankline.lua
@@ -26,13 +26,13 @@ return {
     local hooks = require 'ibl.hooks'
 
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
-      vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
-      vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
-      vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
-      vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
-      vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
-      vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+      vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#DFCDCF' })
+      vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E7E3DA' })
+      vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#CBD7E2' })
+      vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D5CBC3' })
+      vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#CBD2C6' })
+      vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#DACEDF' })
+      vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#ADC0C3' })
     end)
 
     require('ibl').setup(opts)
